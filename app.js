2acffe5125d9945195be94a049802422acd0dba2
gabriel <gsilvac@indracompany.com>
2021-05-08 18:36:31 -0300

    added the gitingore

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ app.use('/user', userRoutes)
 
 mongoose.connect('mongodb://localhost:27017/qualicorp', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(result => {
-        app.listen(8080);
+        app.listen(5005);
     })
     .catch(err => console.log(err))
